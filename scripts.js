fc728def6ad9062c22fc0b75a48a48cd212229ae
Gregory Josephs <greg.josephs@gmail.com>
2021-12-26 09:12:51 -0500

    commented out touch code

--- scripts.js
+++ scripts.js
@@ -131,7 +131,7 @@ function dragElement(elmnt) {
     
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
     elmnt.onmousedown = dragMouseDown;
-    elmnt.ontouchstart = dragMouseDown;
+    //elmnt.ontouchstart = dragMouseDown;
 
   function dragMouseDown(e) {
     e = e || window.event;
@@ -143,10 +143,10 @@ function dragElement(elmnt) {
     pos3 = e.clientX;
     pos4 = e.clientY;
     document.onmouseup = closeDragElement;
-    document.ontouchend = closeDragElement;
+    //document.ontouchend = closeDragElement;
     // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
-    document.ontouchmove = elementDrag;
+    //document.ontouchmove = elementDrag;
   }
 
   function elementDrag(e) {
